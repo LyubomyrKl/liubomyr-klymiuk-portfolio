79b11b3f157dfb878267094506c66d80a5b3df36
Liubomyr Klymiuk <lyubomyrklymyuk2001@gmail.com>
2024-04-07 15:28:38 +0300

    fix animation

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,15 @@ import "./globals.css";
 import {usePathname, useRouter} from "next/navigation";
 import SplashScreen from "@/app/ui/splash-screen";
 
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
 
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
+import { TextPlugin } from "gsap/TextPlugin";
+
+
+gsap.registerPlugin(useGSAP,ScrollTrigger,ScrollToPlugin,TextPlugin);
 
 export default function RootLayout({
   children,
@@ -18,13 +26,13 @@ export default function RootLayout({
 }>) {
   const pathname = usePathname();
   const router = useRouter();
-
   const initialTitle = pathname === '/' ? 'Home' : pathname === '/projects' ? 'Projects' : pathname === '/experience' ? 'Experience' : 'About';
 
-  const [title, setTitle] = React.useState<string>(`${initialTitle}.`);
-  const [startAnimation, setStartAnimation] = React.useState<boolean>(false);
-  const [startFadeAnimation, setStartFadeAnimation] = React.useState<boolean>(false);
 
+    const splashRef = React.useRef(null);
+    const textSplashRef = React.useRef(null);
+    const bodyRef = React.useRef<HTMLBodyElement>(null);
+    const {contextSafe} = useGSAP();
 
     const [isLoading, setIsLoading] = React.useState(true);
 
@@ -36,43 +44,62 @@ export default function RootLayout({
 
 
 
-    const navigate = useCallback((link: string, label:string) => {
-      setTitle(`${label}.`);
+    const navigate = contextSafe((link: string, label:string) => {
 
-      setStartAnimation(true);
+        if(bodyRef.current){
+            bodyRef.current.style.overflow = 'hidden';
+        }
 
-      setTimeout(() => {
-            setStartFadeAnimation(true);
-      }, 100);
+        const tl = gsap.timeline();
+        tl.to(bodyRef.current, {
+            overflow: 'hidden',
+        })
+        .to(splashRef.current, {
+            top: 'initial',
+            bottom: '0',
+            height: '100vh',
+            duration: 1.6,
+            ease: "power4.out",
+            onComplete: () => {
+                router.push(link);
+            }
+        }).to(textSplashRef.current, {
+            opacity: 1,
+            duration: .7,
+        }, '-=1.6')
+        .to(textSplashRef.current, {
+            text: `${label}.`,
+        })
+        .to(textSplashRef.current, {
+            opacity: 0,
+            duration: .7,
+            delay: .3,
+        })
+        .to(splashRef.current,{
+            bottom: 'initial',
+            top: '0',
+            height: '0',
+            duration: 1.6,
+            ease: "power4.out",
+        }, '-=.7')
+        .to(bodyRef.current, {
+            overflow: 'visible',
+        })
 
-        setTimeout(() => {
-            setStartFadeAnimation(false);
-        }, 1500);
-
-        setTimeout(() => {
-            setStartAnimation(false);
-        }, 1600);
-
-        setTimeout(() => {
-            router.push(link);
-        }, 1500);
-  }, [router]);
+    })
 
 
   return (
     <html lang="en">
-      <body className={`${quicksand.className} min-h-screen bg-app-background${!isLoading ? ' pb-24' : ''}${startAnimation ? ' overflow-hidden' : ''}`}>
+      <body ref={bodyRef} className={`${quicksand.className} min-h-screen bg-app-background`}>
       {isLoading ? <SplashScreen finishLoading={() => setIsLoading(false)}/> : (
         <>
             {children}
             <div className='fixed bottom-0 flex items-center min-w-full justify-center left-0 right-0 sm:p-10 py-10'>
                 <GlobalNav onLinkClick={navigate}/>
             </div>
-            <div className={`absolute h-[0px] w-full bottom-0 left-0 bg-app-black flex justify-center items-center  
-            ${startAnimation ? 'animate-popup-route-in' : 'animate-popup-route-out'}`}>
-                <h1 className={`text-7xl text-app-white
-                    ${startFadeAnimation ? 'animate-fadeIn' : 'animate-fadeOut'}
-                `}>{title}</h1>
+            <div ref={splashRef} className={`fixed h-[0px] w-full bottom-0 left-0 bg-app-black flex justify-center items-center`}>
+                <h1 ref={textSplashRef} id='splash-title' className={`text-7xl text-app-white opacity-0`}>{initialTitle}.</h1>
             </div>
         </>
           )}
