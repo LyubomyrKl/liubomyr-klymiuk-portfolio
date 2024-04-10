5f8efb1dbd2452f4f34382e598404da5e60ec63b
Liubomyr Klymiuk <lyubomyrklymyuk2001@gmail.com>
2024-04-10 14:48:43 +0300

    remove click project from mobile

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -81,7 +81,7 @@ const Page = () => {
 
     return (
         <div className="relative w-full h-screen overflow-hidden">
-            {items.map((item, index) => <ProjectItem key={item.title} onClick={(index === 2) || (index === 3)  ? nextItem : undefined} {...item}/>)}
+            {items.map((item, index) => <ProjectItem key={item.title} onClick={((index === 2) || (index === 3)) && pageWidth > 640  ? nextItem : undefined} {...item}/>)}
         </div>
     );
 };
